94a963c1fb32b7d615ca109973e57ed492c45c42
kmdharmaprakash <kmdharmaprakash@gmail.com>
2020-02-15 22:53:34 -0800

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,8 @@
         <div class="col-md-8">
             <div class="card">
 
+            
+
                 @if(count($errors)>0)
                         @foreach($errors->all() as $error)
                         <div class="alert alert-danger">{{$error}}</div>
@@ -16,7 +18,7 @@
                         <div class="alert alert-success">{{session('response')}}</div>
                     @endif
 
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Dashboards</div>
                 <div class="panel-body">
                     <div class="col-md-4">
                         {{----}}
